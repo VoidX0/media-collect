4ff10174b6270755188c4edf7860b1b9b0498bbc
VoidX0 <2982927629@qq.com>
2026-04-27 16:22:14 +0800

    feat: convert download tasks sorting to a list for improved performance

--- api/MediaCollect/Controllers/CollectMediaController.cs
+++ api/MediaCollect/Controllers/CollectMediaController.cs
@@ -154,7 +154,9 @@ public class CollectMediaController : OrmController<CollectedMedia>
     public async Task<ActionResult<List<DownloadTask>>> DownloadTasks()
     {
         await Task.CompletedTask;
-        var result = App.DownloadTasks.OrderBy(x => x.Series).ThenByDescending(x => x.Progress);
+        var result = App.DownloadTasks
+            .OrderBy(x => x.Series).ThenByDescending(x => x.Progress)
+            .ToList();
         return Ok(result);
     }
 
