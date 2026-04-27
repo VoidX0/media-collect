94f248dae4970d4328ec19cf65bc037eb17845f3
VoidX0 <2982927629@qq.com>
2026-04-27 16:19:57 +0800

    feat: sort download tasks by series and progress for improved organization

--- api/MediaCollect/Controllers/CollectMediaController.cs
+++ api/MediaCollect/Controllers/CollectMediaController.cs
@@ -154,7 +154,7 @@ public class CollectMediaController : OrmController<CollectedMedia>
     public async Task<ActionResult<List<DownloadTask>>> DownloadTasks()
     {
         await Task.CompletedTask;
-        var result = App.DownloadTasks.ToList();
+        var result = App.DownloadTasks.OrderBy(x => x.Series).ThenByDescending(x => x.Progress);
         return Ok(result);
     }
 
